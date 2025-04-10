05f044789e3128e599f3a2d6e3f17c06ce15b61d
huytng <huytng@U-3T9RX5UYV0LEP.corp.amazonworkspaces.com>
2025-04-10 10:18:06 -0700

    test

--- terraform/development.tf
+++ terraform/development.tf
@@ -1,23 +1,23 @@
 # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-module "sandbox_account_03" {
+module "test_account_1" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "huytng-1219@amazon.com"
-    AccountName  = "sandbox-account-3"
+    AccountEmail = "huytng-121989@amazon.com"
+    AccountName  = "dev-1"
     # Syntax for top-level OU
     ManagedOrganizationalUnit = "Sandbox"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "huytng-1219@amazon.com"
+    SSOUserEmail     = "huytng-121989@amazon.com"
     SSOUserFirstName = "Huy"
     SSOUserLastName  = "Nguyen"
   }
 
   account_tags = {
-    "ABC:Owner"       = "huytng-1219@amazon.com"
+    "ABC:Owner"       = "huytng-121989@amazon.com"
     "ABC:Division"    = "ENT"
     "ABC:Environment" = "Dev"
     "ABC:CostCenter"  = "123456"
