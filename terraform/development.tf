1cf9ba894c185b307d96d673ac916bc10e703d37
huytng <huytng@U-3T9RX5UYV0LEP.corp.amazonworkspaces.com>
2025-04-10 09:11:06 -0700

    test

--- terraform/development.tf
+++ terraform/development.tf
@@ -5,19 +5,19 @@ module "sandbox_account_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "huy.nguyen@gmail.com"
-    AccountName  = "dev-account-1"
+    AccountEmail = "huytng-1219@amazon.com"
+    AccountName  = "sandbox-account-1"
     # Syntax for top-level OU
     ManagedOrganizationalUnit = "Sandbox"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "huy.nguyen@gmail.com"
+    SSOUserEmail     = "huytng-1219@amazon.com"
     SSOUserFirstName = "Huy"
     SSOUserLastName  = "Nguyen"
   }
 
   account_tags = {
-    "ABC:Owner"       = "huy.nguyen@amazon.com"
+    "ABC:Owner"       = "huytng-1219@amazon.com"
     "ABC:Division"    = "ENT"
     "ABC:Environment" = "Dev"
     "ABC:CostCenter"  = "123456"
