dc372e76ad2534e73bd7238cee17815c242cf44a
huytng <huytng@U-3T9RX5UYV0LEP.corp.amazonworkspaces.com>
2025-04-14 18:34:53 -0700

    security

--- terraform/account_import.tf
+++ terraform/account_import.tf
@@ -34,4 +34,36 @@ module "aft-import-ct-management" {
   account_customizations_name = "aft-ct-mgmt"
 }
 
+module "aft-security-audit" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "huytng+audit123@amazon.com"
+    AccountName  = "Audit"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Security"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "huytng+audit123@amazon.com"
+    SSOUserFirstName = "Huy"
+    SSOUserLastName  = "Nguyen"
+  }
+
+  account_tags = {
+    owner       = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Huy Nguyen"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    tags = jsonencode({
+        env = "shared"
+    })
+  }
+
+  account_customizations_name = "aft-security"
+}
 
