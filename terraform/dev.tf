a7ded8df82508171e9d49f5bddf0e98ee4b54c0f
Huy Nguyen <61076435+huytn1219@users.noreply.github.com>
2025-05-09 21:26:41 -0700

    Update dev.tf
    Signed-off-by: Huy Nguyen <61076435+huytn1219@users.noreply.github.com>

--- terraform/dev.tf
+++ terraform/dev.tf
@@ -39,3 +39,43 @@ module "test_account_1" {
 
   account_customizations_name = "development-customizations"
 }
+
+
+module "test_account_2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "huytng-12198911@amazon.com"
+    AccountName  = "dev-2"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Sandbox"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "huytng-12192189@amazon.com"
+    SSOUserFirstName = "Huy"
+    SSOUserLastName  = "Nguyen"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "huytng-121989@amazon.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Huy Nguyen"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "development-customizations-2"
+}
